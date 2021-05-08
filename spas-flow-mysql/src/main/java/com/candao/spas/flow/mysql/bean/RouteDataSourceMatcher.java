e3ab434b5546c9461caa663c583dce2a0f5ff3c7
jeromeliu <>
2021-05-08 14:31:23 +0800

    更新

--- spas-flow-mysql/src/main/java/com/candao/spas/flow/mysql/bean/RouteDataSourceMatcher.java
+++
@@ -1,8 +0,0 @@
-package com.candao.spas.flow.mysql.bean;
-
-import java.util.List;
-
-public interface RouteDataSourceMatcher {
-	void refresh(List<RouteDataSourceConfig> routeDataSourceConfigs);
-	String match(String matchPath);
-}
--- spas-flow-redis/src/main/java/com/candao/spas/flow/redis/data/RedisCall.java
+++
@@ -1,8 +0,0 @@
-package com.candao.spas.flow.redis.data;
-
-import redis.clients.jedis.Jedis;
-
-public interface RedisCall<T> {
-	
-	T execute(Jedis jedis) ;
-}
